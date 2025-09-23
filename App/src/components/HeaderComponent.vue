e229c5b34353cd8cd5a9f3d333746fcfb536951d
JuanInnovaIT <juanmanuel.sanchez@innovait.cat>
2025-09-24 00:27:39 +0200

    Refactor header navigation links for improved readability and maintainability

--- App/src/components/HeaderComponent.vue
+++ App/src/components/HeaderComponent.vue
@@ -3,23 +3,39 @@
     <q-toolbar class="toolbar">
       <div class="header-left">
         <img src="../assets/logo.png" alt="Logo" class="logo" />
-  <span class="app-name centered-secondary">{{ t('header.app_name') }}</span>
+        <span class="app-name centered-secondary">{{ t('header.app_name') }}</span>
       </div>
       <div class="header-center">
         <nav class="header-nav">
-          <a class="nav-link" href="" @click="handleNavClick('home', $event)">{{ t('header.home') }}</a>
-          <a class="nav-link" href="" @click="handleNavClick('work', $event)">{{ t('header.our_work') }}</a>
+          <a class="nav-link" href="" @click="handleNavClick('home', $event)">{{
+            t('header.home')
+          }}</a>
+          <a class="nav-link" href="" @click="handleNavClick('work', $event)">{{
+            t('header.our_work')
+          }}</a>
           <router-link class="nav-link" to="/whoami">{{ t('header.who_am_i') }}</router-link>
-          <a class="nav-link" href="" @click="handleNavClick('success', $event)">{{ t('header.success_cases') }}</a>
-          <a class="nav-link" href="" @click="handleNavClick('faq', $event)">{{ t('header.faq') }}</a>
-          <a class="nav-link" href="" @click="handleNavClick('contact', $event)">{{ t('header.contact') }}</a>
+          <a class="nav-link" href="" @click="handleNavClick('success', $event)">{{
+            t('header.success_cases')
+          }}</a>
+          <a class="nav-link" href="" @click="handleNavClick('faq', $event)">{{
+            t('header.faq')
+          }}</a>
+          <a class="nav-link" href="" @click="handleNavClick('contact', $event)">{{
+            t('header.contact')
+          }}</a>
         </nav>
       </div>
       <div class="header-right">
         <q-btn-dropdown split rounded icon="language" class="language-select">
           <template #label>
             <span class="label">
-              {{ configStore.language === 'es-ES' ? 'ES' : configStore.language === 'ca-ES' ? 'CA' : 'EN' }}
+              {{
+                configStore.language === 'es-ES'
+                  ? 'ES'
+                  : configStore.language === 'ca-ES'
+                    ? 'CA'
+                    : 'EN'
+              }}
             </span>
           </template>
           <q-list>
@@ -37,7 +53,8 @@
         <q-btn rounded @click="toggleTheme" class="theme-btn">
           {{ configStore.currentTheme === 'light' ? t('header.sun') : t('header.moon') }}
         </q-btn>
-  <q-btn v-if="!login" rounded class="login-btn">{{ t('header.login') }}</q-btn>
+        <!-- TODO: proximo desarrollo de login -->
+        <!-- <q-btn v-if="!login" rounded class="login-btn">{{ t('header.login') }}</q-btn> -->
         <q-btn round v-if="login">
           <q-avatar>
             <img
@@ -54,7 +71,6 @@
 import { useConfigStore } from '../stores/settingsStore';
 import { useI18n } from 'vue-i18n';
 import { useRouter, useRoute } from 'vue-router';
-
 
 const { t, locale } = useI18n();
 const configStore = useConfigStore();
